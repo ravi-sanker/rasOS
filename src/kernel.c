5f5ff3be65361142ad423647d8539ef920a7534f
ravi-sanker <sravisanker16@gmail.com>
2025-08-26 19:21:00 +0530

    Setup printing to screen

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,60 @@
 #include "kernel.h"
+#include <stdint.h>
+#include <stddef.h>
 
-void
-kernel_main() {
+uint16_t *video_mem = 0;
+uint16_t terminal_current_row = 0;
+uint16_t terminal_current_col = 0;
 
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while(str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+uint16_t terminal_make_char(char c, char colour) {
+    // colour needs to come to the left due to the little endian format.
+    return (colour << 8) | c;
+}
+
+void terminal_putchar(int x, int y, char c, char colour) {
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
+}
+
+void terminal_default_putchar(char c, char colour) {
+    if (c == '\n') {
+        terminal_current_row += 1;
+        terminal_current_col = 0;
+        return;
+    }
+
+    terminal_putchar(terminal_current_col, terminal_current_row, c, colour);
+    terminal_current_col += 1;
+    if (terminal_current_col >= VGA_WIDTH) {
+        terminal_current_col = 0;
+        terminal_current_row += 1;
+    }
+}
+
+void terminal_print(const char* str) {
+    size_t len = strlen(str);
+    for(int i = 0; i < len; i++) {
+        terminal_default_putchar(str[i], 15);
+    }
+}
+
+void terminal_initialize() {
+    video_mem = (uint16_t*)(VIDEO_MEM_ADDR);
+    for(int y = 0; y < VGA_HEIGHT; y++) {
+        for(int x = 0; x < VGA_WIDTH; x++) {
+            terminal_putchar(x, y, ' ', 0);
+        }
+    }
+}
+
+void kernel_main() {
+    terminal_initialize();
+    terminal_print("Hello, World!\nI'm Ravi.");
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,10 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 20
+#define VIDEO_MEM_ADDR 0xB8000
+
 void kernel_main();
 
 #endif
